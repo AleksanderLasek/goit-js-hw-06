fc05400b660a458a3012e7d061eee1cdb6503ef4
Aleksander Lasek <aleksanderlasek01@gmail.com>
2022-04-06 00:10:32 +0200

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,24 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const $ingredientsList = document.querySelector("ul");
+
+let $li;
+const elementsArray = [];
+
+for (let i = 0; i < ingredients.length; i++) {
+  $li = document.createElement("li");
+  $li.textContent = ingredients[i];
+  $li.classList.add("item");
+  elementsArray.push($li);
+}
+
+for (const ingredient of elementsArray) {
+  $ingredientsList.append(ingredient);
+}
