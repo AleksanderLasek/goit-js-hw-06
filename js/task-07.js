cf29e02ec3c7ab0ad0ccb0fe492947b6c0ea1104
Aleksander Lasek <aleksanderlasek01@gmail.com>
2022-04-06 02:29:59 +0200

    sprzatanie kodu

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,5 @@
 const $input = document.querySelector("#font-size-control");
-console.log($input);
 const $text = document.querySelector("#text");
-console.log($text);
 
 $input.addEventListener("input", (event) => {
   $text.style.fontSize = `${event.currentTarget.value}px`;
