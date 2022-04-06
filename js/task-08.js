03517ff63eced0218793f303564f87577383d1b5
Aleksander Lasek <aleksanderlasek01@gmail.com>
2022-04-06 02:01:08 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const $form = document.querySelector("form");
 
+$form.addEventListener("submit", submitForm);
+
+function submitForm(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("Wszystkie pola muszą być wypełnione!");
+  }
+
+  const data = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(data);
+  $form.reset();
+}
