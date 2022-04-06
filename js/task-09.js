f0d0d843866c0037c91b70056788b849c7d11318
Aleksander Lasek <aleksanderlasek01@gmail.com>
2022-04-06 02:10:49 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const $colorChangingButton = document.querySelector(".change-color");
+const $body = document.querySelector("body");
+const $colorName = document.querySelector(".color");
+
+$colorChangingButton.addEventListener("click", changeBackgroundColor);
+
+function changeBackgroundColor() {
+  let newColor = getRandomHexColor();
+  $body.style.backgroundColor = getRandomHexColor();
+  $colorName.textContent = newColor;
+}
